8b9a0bcd38274a7780398ac08d9a1cc6a258c8ff
amanAtGit09 <18.aman.singh.18@gmail.com>
2026-03-09 01:46:25 +0530

    deleted unused files and directories created during sam configuration

--- backend/tests/unit/local_test.py
+++
@@ -1,9 +0,0 @@
-from app import lambda_handler
-
-# Mock the AWS event and context
-event = {}
-context = {}
-
-print("--- Starting Local Test (No Docker) ---")
-result = lambda_handler(event, context)
-print(result)
